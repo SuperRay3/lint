9ebba2ca0afb4ffcdefeba5f9214ba5819453496
leijialiang <ljl519525771@qq.com>
2022-07-26 16:54:42 +0800

    build: reomve commitizen

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,11 +3,6 @@ module.exports = {
   rules: {
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
-    ]
+    'subject-empty': [2, 'never']
   }
 }
